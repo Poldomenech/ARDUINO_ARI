8b896facaa4c58da1e92050a3ad940f2d028cfec
Pol Domenech <pol.domenech.37@lacetania.cat>
2021-03-25 09:59:18 +0100

    Update prog7_8leds.ino

--- Array de 8 LEDs/prog7_8leds/prog7_8leds.ino
+++ Array de 8 LEDs/prog7_8leds/prog7_8leds.ino
@@ -1,12 +1,12 @@
 
-unsigned long t=200;   //temps de delay
+unsigned long t=500;   //temps de delay
 const int led0=10;     //assignacio pin 10 com a sortida pel led
 const int B1Pin=2;     //assignacio pin 2 per entrada boto 1
 int B1State=0;         //per guardar estat del boto 1
 const int B2Pin=3;     //assignacio pin 2 per entrada boto 2
 int B2State=0;         //per guardar estat del boto 2
 int i=0;               //constant per controlar for loops
-int w=30;               //constant per controlar while loop
+int w=30;              //constant per controlar while loop
 
 void setup()
 {
@@ -14,6 +14,14 @@ Serial.begin(9600);
 pinMode(led0,OUTPUT);
 pinMode(B1Pin, INPUT);
 pinMode(B2Pin, INPUT);
+  for(int i=0; i<w; i++)
+  {
+  digitalWrite(led0,1);
+  Serial.println(i);
+  delay(t);
+  digitalWrite(led0,0);
+  delay(t);
+  }
 }
 
 void loop() 
@@ -30,7 +38,7 @@ if (B1State==1&&B2State==0)
 }
 else if(B1State==0&&B2State==1)
 {
-while( i<w)
+while(i<w)
   digitalWrite(led0,1);
   delay(t);
   digitalWrite(led0,0);
