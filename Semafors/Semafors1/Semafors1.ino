d06e7031f5af9b664b9e01e124723a051aa1c1e7
Pol Domenech <pol.domenech.37@lacetania.cat>
2021-03-28 11:11:34 +0200

    Update Semafors1.ino

--- Semafors/Semafors1/Semafors1.ino
+++ Semafors/Semafors1/Semafors1.ino
@@ -10,6 +10,8 @@
 const int ledPin[]={7,8,9,10,11,12};              //definir noms pels pins de sortida
 int ledNum=6;                                     //definir variable de numero de Leds
 unsigned long ARon=0;                             //TON de semafor A RED
+int ARtimon=11000;                                //Temps que Led RED semafor A esta en ON
+int ARtimoff=9000;                                //Temps que Led RED semafor A esta en OF
 unsigned long AOon=10;                            //TON de semagor A ORANGE
 unsigned long AGon=10;                            //TON de semafor A GREEN
 unsigned long BRon=0;                             //TON de semafor B RED
@@ -20,12 +22,12 @@ int sortides=9;                                   //sortides en vinari
 
 //********** Setup ****************************************************************
 void setup() {
-  Serial.begin(9600);                                 //definir velocitat
-  for(int i = 0; i < ledNum; i++)                     // definir els pins 7,8,9,10,11 i 12 com sortides
+  Serial.begin(9600);                              //definir velocitat
+  for(int i = 0; i < ledNum; i++)                  // definir els pins 7,8,9,10,11 i 12 com sortides
   {
   pinMode(ledPin[i], OUTPUT);
   }
-  for(int j = 0; j < ledNum ; j++)                    // posar Leds a 0
+  for(int j = 0; j < ledNum ; j++)                 // posar Leds a 0
   {
   digitalWrite(ledPin[j], 0);
   }
