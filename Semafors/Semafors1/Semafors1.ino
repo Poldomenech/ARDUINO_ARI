065e9ab78ac0f0a98128040e646c7ff65e14d769
Pol Domenech <pol.domenech.37@lacetania.cat>
2021-03-28 11:12:34 +0200

    Binari millor

--- Semafors/Semafors1/Semafors1.ino
+++ Semafors/Semafors1/Semafors1.ino
@@ -11,14 +11,14 @@ const int ledPin[]={7,8,9,10,11,12};              //definir noms pels pins de so
 int ledNum=6;                                     //definir variable de numero de Leds
 unsigned long ARon=0;                             //TON de semafor A RED
 int ARtimon=11000;                                //Temps que Led RED semafor A esta en ON
-int ARtimoff=9000;                                //Temps que Led RED semafor A esta en OF
+int ARtimoff=9000;                                //Temps que Led RED semafor A esta en OFF
 unsigned long AOon=10;                            //TON de semagor A ORANGE
 unsigned long AGon=10;                            //TON de semafor A GREEN
 unsigned long BRon=0;                             //TON de semafor B RED
 unsigned long BOon=0;                             //TON de semafor B ORANGE
 unsigned long BGon=0;                             //TON de semafor B GREEN
 
-int sortides=9;                                   //sortides en vinari
+int sortides=9;                                   //sortides en binari
 
 //********** Setup ****************************************************************
 void setup() {
