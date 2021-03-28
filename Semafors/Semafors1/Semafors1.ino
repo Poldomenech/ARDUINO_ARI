772cd89d97c0c5bef89460bdb5860f144770111b
Pol Domenech <pol.domenech.37@lacetania.cat>
2021-03-28 13:01:48 +0200

    Update Semafors1.ino

--- Semafors/Semafors1/Semafors1.ino
+++ Semafors/Semafors1/Semafors1.ino
@@ -10,8 +10,9 @@
 const int ledPin[]={7,8,9,10,11,12};              //definir noms pels pins de sortida
 int ledNum=6;                                     //definir variable de numero de Leds
 unsigned long ARon=0;                             //TON de semafor A RED
+unsigned long ARoff=12000;                        //TOFF de semafor A RED
 int ARtimon=11000;                                //Temps que Led RED semafor A esta en ON
-int ARtimoff=9000;                                //Temps que Led RED semafor A esta en OFF
+int ARtimoff=9000;                               //Temps que Led RED semafor A esta en OFF
 unsigned long AOon=10;                            //TON de semagor A ORANGE
 unsigned long AGon=10;                            //TON de semafor A GREEN
 unsigned long BRon=0;                             //TON de semafor B RED
@@ -23,18 +24,32 @@ int sortides=9;                                   //sortides en binari
 //********** Setup ****************************************************************
 void setup() {
   Serial.begin(9600);                              //definir velocitat
-  for(int i = 0; i < ledNum; i++)                  // definir els pins 7,8,9,10,11 i 12 com sortides
-  {
+  for(int i = 0; i < ledNum; i++){                  // definir els pins 7,8,9,10,11 i 12 com sortides
+  
   pinMode(ledPin[i], OUTPUT);
   }
-  for(int j = 0; j < ledNum ; j++)                 // posar Leds a 0
-  {
+  for(int j = 0; j < ledNum ; j++){                // posar Leds a 0
+  
   digitalWrite(ledPin[j], 0);
   }
 }
 
 //********** Loop *****************************************************************
 void loop() {
-  
+if (millis()-ARon<=ARtimon){                               //encesa Led RED semafor A
+  digitalWrite(7,1);
+}
+else if(millis()-ARon>ARtimon && (millis()-ARon<=ARtimoff+ARtimon)){
+  digitalWrite(7,0);
   
 }
+else if (millis()-(ARtimoff+ARtimon)>0){
+  ARon=millis();
+  Serial.println(ARon);
+}
+}
+
+
+  
+  
+  
