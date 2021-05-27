0da2e1b6080b768f10e10f45465f458b331aaa3d
Pol Domenech <pol.domenech.37@lacetania.cat>
2021-05-27 23:57:29 +0200

    Update bomba1.0.ino

--- bomba1.0/bomba1.0.ino
+++ bomba1.0/bomba1.0.ino
@@ -8,7 +8,7 @@
 //********** Includes *************************************************************
 //********** Variables ************************************************************
 #include <Keypad.h> 
-
+char TECLA;
 const byte FILAS=4;
 const byte COLUMNAS=4;
 char keys[FILAS][COLUMNAS]=
@@ -20,16 +20,35 @@ char keys[FILAS][COLUMNAS]=
 };
 byte pinesFilas[FILAS] = {9,8,7,6};   
 byte pinesColumnas[COLUMNAS] = {5,4,3,2}; 
+char PASS_RANDOM[7]={1,2,3,4,5,6};
+char PASS[7];
+byte INDICE=0;
+Keypad teclat=Keypad(makeKeymap(keys),pinesFilas, pinesColumnas, FILAS, COLUMNAS);
+
 
 void setup() {
 randomSeed(analogRead(A0)); 
 Serial.begin(9600);
-
-Keypad teclado=Keypad(makeKeymap(keys),pinesFilas, pinesColumnas, FILAS, COLUMNAS);
-
+Serial.println (" ");
+Serial.print ("PASS ");
+for(int i = 0; i < 6 ; i++)
+{                
+  PASS_RANDOM[i]=random(1,10);
+}
+  for (int i=0; i<7;i++)
+  {
+  INDICE=PASS_RANDOM[i];
+  Serial.print(INDICE);
+  }
+  
 }
 
-void loop() {
 
+
+
+
+
+void loop() {
+TECLA=teclat.getKey();
 
 }
