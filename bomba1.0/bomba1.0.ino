2851928b6c3564ce5e0a50aa11309dfb4c21fc9c
Pol Domenech <pol.domenech.37@lacetania.cat>
2021-05-27 23:09:12 +0200

    codi per arm

--- bomba1.0/bomba1.0.ino
+++ bomba1.0/bomba1.0.ino
@@ -7,13 +7,25 @@
 **********************************************************************************/
 //********** Includes *************************************************************
 //********** Variables ************************************************************
-unsigned long activation;
+#include <Keypad.h> 
+
+const byte FILAS=4;
+const byte COLUMNAS=4;
+char keys[FILAS][COLUMNAS]=
+{
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+byte pinesFilas[FILAS] = {9,8,7,6};   
+byte pinesColumnas[COLUMNAS] = {5,4,3,2}; 
 
 void setup() {
 randomSeed(analogRead(A0)); 
 Serial.begin(9600);
-activation= random(100000,999999);
-Serial.println(activation);
+
+Keypad teclado=Keypad(makeKeymap(keys),pinesFilas, pinesColumnas, FILAS, COLUMNAS);
 
 }
 
